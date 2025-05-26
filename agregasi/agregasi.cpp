f260dac932b404522cf080c43269e9585d14e764
tari2025 <denilestari272@gmail.com>
2025-05-26 14:19:42 +0700

    agregasi

---
+++ agregasi/agregasi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "anak.h"
+#include "ibu.h"
+
+#int main() { // agregasi}
+ibu *varIbu = newibu("dini");
+ibu *varibu2 = new ibu("novi");
+anak *varAnak1 = new anak("tono");
+anak *varAnak2 = new anak("rini");
+anak *varAnak3 = new anak("Dewi");
+
+varIbu->
