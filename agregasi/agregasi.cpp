66bc978e401dede09db71527863d9c4d41c7ee7a
tari2025 <denilestari272@gmail.com>
2025-05-26 14:25:40 +0700

    daftar anak

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -3,11 +3,27 @@
 #include "anak.h"
 #include "ibu.h"
 
-#int main() { // agregasi}
-ibu *varIbu = newibu("dini");
-ibu *varibu2 = new ibu("novi");
-anak *varAnak1 = new anak("tono");
-anak *varAnak2 = new anak("rini");
-anak *varAnak3 = new anak("Dewi");
+int main()
+{ // agregasi}
+    ibu *varIbu = new ibu("dini");
+    ibu *varibu2 = new ibu("novi");
+    anak *varAnak1 = new anak("tono");
+    anak *varAnak2 = new anak("rini");
+    anak *varAnak3 = new anak("Dewi");
 
-varIbu->
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varibu2->cetakAnak();
+
+    delete varIbu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    return 0;
+}
